97a15a9f4068959c751df80a605bda50c608b0c6
Adrian Irving-Beer <wisq@wisq.net>
2015-01-29 22:54:51 -0500

    Fix duplicate env dir resource.

--- manifests/service/env.pp
+++ manifests/service/env.pp
@@ -24,18 +24,9 @@ define runit::service::env( $service, $envname = $title, $value, $ensure = prese
 
   $envdir = "/etc/sv/${service}/env"
 
-  # create this directory if at least one environment variable is defined
-  if $ensure == present and !defined( File["/etc/sv/${service}/env"] ) {
-    file{ "/etc/sv/${service}/env":
-      ensure  => directory,
-      # all unmanaged envs. will be removed
-      recurse => true,
-      purge   => true,
-    }
-  }
-
   file { "${envdir}/${envname}":
     ensure => $ensure,
     content => "${value}\n",
   }
+
 }
